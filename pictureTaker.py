09606acb7bc27573a5cb2d622914625d110b6844
Nicholas Liu <nicholas.j.liu@gmail.com>
2018-01-05 00:29:07 -0800

    add comments

--- pictureTaker.py
+++ pictureTaker.py
@@ -1,8 +1,10 @@
 import cv2
 import keyboard
 
+#set folder for where the pictures will be saved
 folderDirectory = "pics/"
 
+#Naming conventions for the pictures.
 def filename(fnum):
 	if fnum < 10:
 		fname = folderDirectory + "000" + str(fnum) + ".jpg"
@@ -20,6 +22,7 @@ def main():
 	cap = cv2.VideoCapture(0)
 	filenum = 0
 
+	#Iterate to the next picture name
 	while True:
 		try:
 			f = open(filename(filenum))
@@ -28,14 +31,17 @@ def main():
 		except IOError as e:
 			break
 
+	#Loop to capture and display feed
 	while cap.isOpened():
 		
 		_,frame = cap.read()
 		cv2.imshow('frame', frame)		
 		
+		#use to capture picture
 		if cv2.waitKey(1) & 0xFF == ord('p'):
 			cv2.imwrite(filename(filenum), frame)
  			filenum += 1
+		#this doesn't work. Just use CTRL+C for now
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 		        break
 
